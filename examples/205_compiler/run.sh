28902898678af106a4998b08d725260db4182188
Martin Janiczek <martin@janiczek.cz>
2024-10-08 19:43:44 +0200

    Fix the git test

--- examples/205_compiler/run.sh
+++ examples/205_compiler/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-elm-bench --compiler /Users/martinjaniczek/.volta/bin/elm -f RemoveOld.remove -f RemoveNew.remove 42 "List.range 0 1000"
+elm-bench --compiler /Users/martin/.yarn/bin/elm -f RemoveOld.remove -f RemoveNew.remove 42 "List.range 0 1000"
--- examples/301_git_singleton/run.sh
+++ examples/301_git_singleton/run.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 # old-version | remove-old | f8a0681c8196c0d640f145938dbf8e33897a16f4
 # master      | remove-new | b6be88de1ee5e3985338209b6fa37a4decc66b8c
-elm-bench -g remove-old -g remove-new -g old-version -g master -g f8a0681 -g b6be88d Remove.remove 42 "List.range 0 1000"
+rm -rf .git
+git init .
+git checkout -b master
+mkdir -p src
+cp RemoveOld.elm src/Remove.elm
+git add src/Remove.elm
+git commit -m "old"
+HASH_OLD=$(git rev-parse HEAD)
+git tag remove-old
+git checkout -b old-version
+git checkout master
+cp RemoveNew.elm src/Remove.elm
+git add src/Remove.elm
+git commit -m "new"
+HASH_NEW=$(git rev-parse HEAD)
+git tag remove-new
+elm-bench -g remove-old -g remove-new -g old-version -g master -g "$HASH_OLD" -g "$HASH_NEW" Remove.remove 42 "List.range 0 1000"
+rm -rf .git
