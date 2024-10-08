c073342c14b14dda38184e6f721acbd11a8b1294
Martin Janiczek <martin.janiczek@vendr.com>
2024-10-08 14:58:43 +0200

    Optimize `remove` function in 301_git_singleton

--- examples/301_git_singleton/src/Main.elm
+++ examples/301_git_singleton/src/Main.elm
@@ -3,13 +3,23 @@ module Remove exposing (remove)
 
 remove : a -> List a -> List a
 remove x xs =
+    removeHelp xs x xs []
+
+
+removeHelp : List a -> a -> List a -> List a -> List a
+removeHelp list x xs previousElements =
     case xs of
         [] ->
-            []
+            list
 
         y :: ys ->
             if x == y then
-                ys
+                reverseAppend previousElements ys
 
             else
-                y :: remove x ys
+                removeHelp list x ys (y :: previousElements)
+
+
+reverseAppend : List a -> List a -> List a
+reverseAppend list1 list2 =
+    List.foldl (::) list2 list1
