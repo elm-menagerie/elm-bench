1f5b469ef65e6796f780724f30225216eee94a24
Martin Janiczek <martin.janiczek@vendr.com>
2024-10-08 16:50:42 +0200

    Make test runner nicer

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -2,4 +2,10 @@
 
 # for every run.sh in this directory, run it
 # (via find)
-find . -name run.sh -execdir bash {} \;
+for x in ./*; do
+  if [ ! -d "${x}" ]; then continue; fi
+  cd "${x}";
+  echo -e "\nRunning test: ${x}";
+  ./run.sh
+  cd ..
+done;
