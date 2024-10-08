208aa2b9e35d7f21ae5bfb4a375ae5c0980940f7
Martin Janiczek <martin.janiczek@vendr.com>
2024-10-08 16:48:57 +0200

    Git worktrees!

--- examples/301_git_singleton/run.sh
+++ examples/301_git_singleton/run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-elm-bench -g HEAD -g tested-branch -g tested-tag -g main Remove.remove 42 "List.range 0 1000"
+# old-version | remove-old | f8a0681c8196c0d640f145938dbf8e33897a16f4
+# master      | remove-new | b6be88de1ee5e3985338209b6fa37a4decc66b8c
+elm-bench -g remove-old -g remove-new -g old-version -g master -g f8a0681 -g b6be88d Remove.remove 42 "List.range 0 1000"
